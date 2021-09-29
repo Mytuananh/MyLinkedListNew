a728221069f8b6474b228ed2c40ce615697b8320
Your Name <you@example.com>
2021-09-30 06:42:25 +0700

    add

---
+++ src/MyLinkedListTest.java
@@ -0,0 +1,11 @@
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList(11);
+        myLinkedList.add(15, 8);
+        myLinkedList.add(18, 6);
+        myLinkedList.add(20, 4);
+        myLinkedList.addFirst(19);
+
+
+    }
+}
